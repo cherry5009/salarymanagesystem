1ad83baa82626796b62215037df604e0f7348dcc
HyukSoa <Soa@zhangxiaomengdeMacBook-Air.local>
2014-06-07 21:06:24 +0800

    Initial Commit

---
+++ salarymanagesystem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  salarymanagesystem
+//
+//  Created by 章小萌 on 14-6-7.
+//  Copyright (c) 2014年 章小萌. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
